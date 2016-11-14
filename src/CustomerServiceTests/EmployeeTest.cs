81e10874b9a6e5eecad392c1bcd65eb1b39e01f9
Elizabeth Sanger <libsang@gmail.com>
2016-11-14 16:27:11 -0600

    first commit

---
+++ src/CustomerServiceTests/EmployeeTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using CustomerServiceConsole;
+
+namespace CustomerServiceTests
+{
+    public class EmployeeTest
+    {
+
+        Employee employee = new Employee
+        {
+            EmployeeId = 123,
+            FirstName = "Bob",
+            LastName = "Dole",
+            DepartmentName = "Accounting"
+
+        };
+
+        [Fact]
+
+        public void TrueTest()
+        {
+            bool IsMonday = true;
+            Assert.True(IsMonday);
+        }
+
+        [Fact]
+
+        public void CanCreateEmployee ()
+        {
+            Assert.NotNull(employee);
+        }
+        
+        [Fact]
+
+        public void EmployeeHasDepartment ()
+        {
+            Assert.NotNull(employee.DepartmentName);
+        }
+    }
+}

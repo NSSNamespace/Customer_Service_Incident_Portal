2b0eb5feaef15eeadff181b9a417f98acf1dbce5
David Yunker <David Yunker>
2016-11-17 13:11:36 -0600

    changes to BangazonConnection file

--- src/CustomerService/BangazonConnection.cs
+++ src/CustomerService/BangazonConnection.cs
@@ -2,10 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
 
 namespace CustomerService
 {
     public class BangazonConnection
     {
+        private string _connectionString = "Data Source=" + System.Environment.GetEnvironmentVariable("Bangazon_Db_Path2");
+
+        public void insert(string query)
+        {
+            SqliteConnection dbcon = new SqliteConnection(_connectionString);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+
+            dbcmd.CommandText = query;
+            dbcmd.ExecuteNonQuery();
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
+
+        public void execute(string query, Action<SqliteDataReader> handler)
+        {
+
+            SqliteConnection dbcon = new SqliteConnection(_connectionString);
+
+            dbcon.Open();
+            SqliteCommand dbcmd = dbcon.CreateCommand();
+            dbcmd.CommandText = query;
+
+            using (var reader = dbcmd.ExecuteReader())
+            {
+                handler(reader);
+            }
+
+            // clean up
+            dbcmd.Dispose();
+            dbcon.Close();
+        }
     }
 }
